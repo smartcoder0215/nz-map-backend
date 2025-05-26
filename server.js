ff01c430bf317c6ab2b884c513c381bb68621535
smartcoder0215 <smartcoder0215@gmail.com>
2025-05-26 06:06:13 -0400

    removed comments

--- server.js
+++ server.js
@@ -32,12 +32,10 @@ app.use('/api/pins', pinsRouter);
 app.use('/api/overlay-images', overlayImagesRouter);
 app.use('/api/pin-icons', pinIconsRouter);
 
-// Basic route for testing
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to Map New Zealand API' });
 });
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
